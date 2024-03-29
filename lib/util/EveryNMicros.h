714a44431500949c68f5bf8efa8a44bbc7e3afcf
Joseph Ntaimo <jntaimo@gmail.com>
2024-03-28 20:27:15 -0400

    Attempted to fix everynMicros

--- lib/util/EveryNMicros.h
+++ lib/util/EveryNMicros.h
@@ -1,26 +1,29 @@
 #ifndef EVERY_N_MICROS_H
-#define EVERY_N_MICROS_H
+#define EVERY_N_MILLIS_H
 
 #include <Arduino.h>
 
 class EveryNMicros {
 public:
-    EveryNMicros(unsigned long interval) : interval(interval), prevMicros(0) {}
+    EveryNMicros(unsigned long interval) : intervalMicros(interval), previousMicros(0) {}
 
-    bool update() {
-        unsigned long currentMicros = micros();
-        if ((unsigned long)(currentMicros - prevMicros) >= interval) {
-            prevMicros = currentMicros;
+    bool shouldRun(unsigned long currentMicros) {
+        if (currentMicros - previousMicros >= intervalMicros) {
+            previousMicros = currentMicros;
             return true;
         }
         return false;
     }
-
+    
 private:
-    unsigned long interval;
-    unsigned long prevMicros;
+    unsigned long previousMicros;
+    unsigned long intervalMicros;
 };
 
-#define EVERY_N_MICROS(n) for (static EveryNMicros _timer(n); _timer.update(); )
+#define CONCAT_INTERNAL(x, y) x##y
+#define CONCAT(x, y) CONCAT_INTERNAL(x, y)
+#define EVERY_N_Micros(N) \
+    static EveryNMicros CONCAT(everyNMicros_, __LINE__)(N); \
+    if (CONCAT(everyNMicros_, __LINE__).shouldRun(micros()))
 
-#endif // EVERY_N_MICROS_H
+#endif
